5fa6ddf4dc65581b642f1e47e81d156600b52652
Johan Nilsson Hansen <Granen@hotmail.com>
2018-04-21 19:42:12 +0200

    Switches emissive via shader instead of changing material

--- Floor.gd
+++ Floor.gd
@@ -6,11 +6,11 @@ var mesh_material
 func _ready():
     mesh = $MeshInstance
     mesh_material = mesh.get_surface_material(0).duplicate()
-    mesh_material.emission_enabled = false
+    mesh_material.set_shader_param("is_emissive", false)
     mesh.set_surface_material(0, mesh_material)
 
 func highlight(should_highlight):
-    mesh_material.emission_enabled = should_highlight
+    mesh_material.set_shader_param("is_emissive", should_highlight)
     
 func retrieve_floor_mesh():
     return mesh
